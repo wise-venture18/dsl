2050fd4deac1ac02a0cd21bf2eda3a2fc3a04efb
wise-venture18 <wiseventure.business@gmail.com>
2024-12-15 19:35:03 +0530

    Create 9.infix-to-postfix.cpp

---
+++ 9.infix-to-postfix.cpp
@@ -0,0 +1,75 @@
+ 
+// In any language program mostly syntax error occurs due to unbalancing 
+// delimiter such as (),{},[]. Write C++ program using stack to check whether 
+// given expression is well parenthesized or not.
+
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+// Function to check precedence of operators
+int prec(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    if (op == '^') return 3;
+    return -1;
+}
+
+// Function to convert infix to postfix
+string infixToPostfix(string s) {
+    stack<char> st;
+    string result;
+    
+    for (int i = 0; i < s.length(); i++) {
+        // If operand, add to result
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) {
+            result += s[i];
+        }
+        // If opening parenthesis, push to stack
+        else if (s[i] == '(') {
+            st.push(s[i]);
+        }
+        // If closing parenthesis, pop and append until '(' is encountered
+        else if (s[i] == ')') {
+            while (!st.empty() && st.top() != '(') {
+                result += st.top();
+                st.pop();
+            }
+            if (!st.empty()) {
+                st.pop();  // Remove the '('
+            }
+        }
+        // If operator, check precedence and pop if necessary
+        else {
+            while (!st.empty() && prec(st.top()) >= prec(s[i])) {
+                result += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+    
+    // Pop any remaining operators in the stack
+    while (!st.empty()) {
+        result += st.top();
+        st.pop();
+    }
+    
+    return result;
+}
+
+// Main function
+int main() {
+    string infix;
+
+    cout << "Enter a valid infix expression: ";
+    cin >> infix;
+
+    // Convert infix to postfix
+    string postfix = infixToPostfix(infix);
+
+    cout << "Postfix Expression: " << postfix << endl;
+
+    return 0;
+}
